3f8d5cc55bd3d95751d365e1e0eb032811e64171
Roman Zabela <cplCold@gmail.com>
2025-07-14 23:03:05 +0300

    added backedn and frontend to repo

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const cors = require('cors');
+const express = require('express');
+
+const fs = require('fs');
+const path = require('path');
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const NOTES_FILE = path.join(__dirname, 'notes.json');
+
+app.get('/api/notes', (req, res) => {
+    console.log(`File directory: ${NOTES_FILE}`);
+    const notes = JSON.parse(fs.readFileSync(NOTES_FILE, 'utf-8'));
+    console.log(`[${new Date().toLocaleString()}]: File opened succsessfully`);
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(NOTES_FILE, 'utf-8'));
+    const newNote = { id: Date.now(), text: req.body.text };
+    notes.push(newNote);
+    fs.writeFileSync(NOTES_FILE, JSON.stringify(notes, null, 2));
+    console.log(`[${new Date().toLocaleString()}]: Note added successfully to the file`);
+    res.status(201).json(newNote);
+});
+
+app.listen(PORT, () => console.log(`[${new Date().toLocaleString()}]: Server started on port ${PORT}`));
---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [notes, setNotes] = useState([]);
+    const [text, setText] = useState("");
+    const API_URL = process.env.REACT_APP_API_URL;
+
+    useEffect(() => {
+        fetch(`${API_URL}/api/notes`)
+            .then(res => res.json())
+            .then(data => setNotes(data));
+    }, []);
+
+    const addNote = () => {
+        fetch(`${API_URL}/api/notes`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ text })
+        })
+        .then(res => res.json())
+        .then(newNote => {
+            setNotes([...notes, newNote]);
+            setText("");
+        });
+    };
+
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>Notes</h1>
+            <input value={text} onChange={e => setText(e.target.value)} />
+            <button onClick={addNote}>Add</button>
+
+            {(!notes || notes.length === 0) ? (
+            <ul><li>No notes loaded</li></ul>
+            ) : (
+            <ul>
+                {notes.map(note => (
+                <li key={note.id}>{note.text}</li>
+                ))}
+            </ul>
+            )}
+        </div>
+    );
+
+}
+
+export default App;
