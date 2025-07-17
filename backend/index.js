cc02e7b7eca01c2627131ad22d54ae10d594592e
Roman Zabela <cplCold@gmail.com>
2025-07-18 00:33:52 +0300

    removed cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
-const cors = require('cors');
 const express = require('express');
 
 const fs = require('fs');
 const path = require('path');
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
